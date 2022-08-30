1132d5c0631542fc60271e3044f352e8d158a9fd
Zachary Boehm <zboehm@menard-inc.com>
2022-08-30 14:08:07 -0500

    Final fix for fas import. Added dynamic links dependant on the page.

--- src/components/admonition/admonition.jsx
+++ src/components/admonition/admonition.jsx
@@ -1,24 +1,21 @@
 import { Component } from 'preact';
+import { FontAwesomeIcon } from '@aduh95/preact-fontawesome';
+
 import "./admonition.scss";
 import "../../scss/main.scss";
-
-import { FontAwesomeIcon } from '@aduh95/preact-fontawesome';
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-
-library.add(fas);
-
 export class Admonition extends Component {
     render(props, state) {
         return (
             <>
                 <div class="admonition mb-6">
-                    <div class="icon">
-                        <FontAwesomeIcon
-                            icon={props.icon === undefined ? "circle-info" : props.icon}
-                            className={props.color === undefined ? "path-blue-gray" : 'path-' + props.color}
-                        />
-                    </div>
+                    {props.icon &&
+                        <div class="icon">
+                            <FontAwesomeIcon
+                                icon={props.icon === undefined ? "circle-info" : props.icon}
+                                className={props.color === undefined ? "path-blue-gray" : 'path-' + props.color}
+                            />
+                        </div>
+                    }
                     <div>
                         {props.children}
                     </div>
