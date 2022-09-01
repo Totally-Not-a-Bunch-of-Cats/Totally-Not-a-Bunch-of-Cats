00cc325360f5fbf08b75f75ddfc50afa6a295554
Zachary Boehm <zboehm@menard-inc.com>
2022-09-01 16:58:50 -0500

    Got blog markdown working with full styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig } from 'astro/config';
-
+import mdx from '@astrojs/mdx';
 import preact from "@astrojs/preact";
+
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://totally-not-a-bunch-of-cats.github.io',
-  base: '/Totally-Not-a-Bunch-of-Cats',
-  integrations: [preact()]
+    site: 'https://totally-not-a-bunch-of-cats.github.io',
+    base: '/Totally-Not-a-Bunch-of-Cats',
+    integrations: [mdx(), preact()],
+    markdown: {
+        shikiConfig: {
+            theme: 'dracula'
+        },
+        rehypePlugins: [
+            [
+                'rehype-external-links',
+                {
+                    target: '_blank',
+                },
+            ],
+        ],
+    }
 });
