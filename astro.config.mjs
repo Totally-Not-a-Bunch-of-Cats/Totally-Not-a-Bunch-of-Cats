b15f14802395280a2283f2c987766b2cec8b9202
Zachary Boehm <zboehm104@gmail.com>
2023-04-27 16:53:43 -0500

    Update for new vercel hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import remarkGfm from 'remark-gfm';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://totally-not-a-bunch-of-cats.github.io',
-    base: '/Totally-Not-a-Bunch-of-Cats',
+    site: 'https://totally-not-a-bunch-of-cats.vercel.app',
+    base: '/',
     integrations: [mdx(), preact({ compat: true })],
     markdown: {
         remarkPlugins: [remarkGfm],
