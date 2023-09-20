0a1028b9a666c36da253162df42363cedab90efc
Zachary <94603332+Tired-Fox@users.noreply.github.com>
2023-09-20 18:03:20 +0000

    Fresh Astro v3 project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import preact from "@astrojs/preact";
-import remarkGfm from 'remark-gfm';
 
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://totally-not-a-bunch-of-cats.vercel.app',
-    base: '/',
-    integrations: [mdx(), preact({ compat: true })],
-    markdown: {
-        remarkPlugins: [remarkGfm],
-        shikiConfig: {
-            theme: 'dracula',
-        },
-    },
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
