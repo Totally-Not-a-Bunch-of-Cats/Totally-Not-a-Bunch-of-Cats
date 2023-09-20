0469e071be5d4e9550c4016d92d29517a92f69dd
Zachary Boehm <zboehm@menard-inc.com>
2023-09-20 15:02:07 -0500

    Remove most of blog template and start rewriting in tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import tailwind from '@astrojs/tailwind';
+import solid from '@astrojs/solid-js';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), tailwind(), solid({
+        // This forces solid js components to only be in the `src/components/solid/` directory
+        include: ['src/components/solid/**/*']
+    })],
 });
